e2f312cf0fda2f15376de90deb880d4ff4d7ea67
chistaam <tampcps38349@gmail.com>
2025-04-09 00:30:58 +0700

    first commit

---
+++ src/main/java/Caculator.java
@@ -0,0 +1,12 @@
+public class Caculator implements ICaculator{
+    @Override
+    public Integer sum(Integer a, Integer b) {
+        return 0;
+    }
+
+    @Override
+    public Integer sub(Integer a, Integer b) {
+        return 0;
+    }
+
+}
