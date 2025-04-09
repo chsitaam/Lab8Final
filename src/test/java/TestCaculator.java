b67351a57ed140f5ab2aa1c3272487632180d59b
chistaam <tampcps38349@gmail.com>
2025-04-09 10:56:43 +0700

    fail

--- src/test/java/TestCaculator.java
+++ src/test/java/TestCaculator.java
@@ -12,11 +12,11 @@ public class TestCaculator {
 
     @Test
     public void testSum(){
-        Assert.assertEquals(caculator.sum(5,5), 0);
+        Assert.assertEquals(caculator.sum(5,5), 10);
     }
 
     @Test
     public void testSubtract(){
-        Assert.assertEquals(caculator.sub(6,6), 1);
+        Assert.assertEquals(caculator.sub(6,6), 11);
     }
 }
